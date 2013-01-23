bd1f58ea7fcb7a5c3f87e3593d82b15bfba7d710
Yossi Kolesnicov <yoxigen@gmail.com>
2013-01-24 00:32:53 +0200

    Added refresh for RSS, styling for header buttons.

--- widgets/rss/rss.controller.js
+++ widgets/rss/rss.controller.js
@@ -1,12 +1,23 @@
-angular.module("Homepage").controller("RssController", ["$scope", "rss", function($scope, rss){
-    function loadFeeds(){
+angular.module("Homepage").controller("RssController", ["$scope", "rss", "$timeout", function($scope, rss, $timeout){
+    var refreshRate = 300,
+        timeoutPromise;
+
+    $scope.loadFeed = function(){
+        $scope.loading = true;
+        $timeout.cancel(timeoutPromise);
+
         rss.load($scope.module.settings.feed).then(function(feeds){
             $scope.feed = feeds[0];
             $scope.moduleStyle = {
                 height: (100 / feeds.length) + "%"
             };
+
+            $scope.loading = false;
+            timeoutPromise = $timeout($scope.loadFeed, refreshRate * 1000);
+        }, function(error){
+            $scope.loading = false;
         });
     }
 
-    loadFeeds();
+    $scope.loadFeed();
 }]);
