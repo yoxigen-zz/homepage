e6aa162891afa48d09f645b33c85118bf8fe5826
Yossi Kolesnicov <yossik@fortscale.com>
2013-09-20 11:49:25 +0300

    Saving selected album to cloud.

--- modules/background/slideshow/slideshow.controller.js
+++ modules/background/slideshow/slideshow.controller.js
@@ -1,11 +1,22 @@
-angular.module("Slideshow").controller("SlideshowController", ["$scope", "$timeout", "utils", "imageCache", "dataImages", "defaultImages", function ($scope, $timeout, utils, imageCache, dataImages, defaultImages) {
+angular.module("Slideshow").controller("SlideshowController", ["$scope", "$timeout", "utils", "imageCache", "dataImages", "defaultImages", "Storage",
+    function ($scope, $timeout, utils, imageCache, dataImages, defaultImages, Storage) {
     var currentImageIndex = 0,
         currentImagePosition = 1,
         images,
-        playTimeoutPromise;
+        playTimeoutPromise,
+        storage = new Storage("slideshow"),
+        configData;
 
-    var slideshowSource = dataImages.flickr;
-    slideshowSource.images.load().then(loadImages);
+    storage.cloud.getItem("config").then(function(data){
+        configData = data;
+        if (configData){
+            var data = configData.getData();
+            $scope.selectSource(dataImages[data.source]);
+            $scope.selectAlbum(data.feed, false);
+        }
+        else
+            loadDefaultFeed();
+    }, loadDefaultFeed);
 
     $scope.currentImages = [{}, {}];
     $scope.play = true;
@@ -42,8 +53,12 @@ angular.module("Slideshow").controller("SlideshowController", ["$scope", "$timeo
             loadSourceItems(source);
     };
 
-    $scope.selectAlbum = function(album){
+    $scope.selectAlbum = function(album, saveToCloud){
         $scope.currentSource.images.load(album).then(loadImages);
+        $scope.currentFeed = album;
+
+        if (saveToCloud !== false)
+            saveConfigToCloud();
     };
 
     $scope.sourceLogin = function(source){
@@ -51,6 +66,22 @@ angular.module("Slideshow").controller("SlideshowController", ["$scope", "$timeo
             loadSourceItems(source);
         });
     };
+
+    function saveConfigToCloud(){
+        if (configData){
+            configData.set("source", $scope.currentSource.name);
+            configData.set("feed", $scope.currentFeed);
+            configData.save();
+        }
+        else{
+            storage.cloud.setItem("config", {
+                source: $scope.currentSource.name,
+                feed: $scope.currentFeed
+            }).then(function(data){
+                configData = data;
+            });
+        }
+    }
 
     function loadImages(imagesData){
         images = utils.arrays.shuffle(imagesData);
@@ -68,6 +99,11 @@ angular.module("Slideshow").controller("SlideshowController", ["$scope", "$timeo
             source.images.getAlbums().then(function(albums){
                 $scope.currentSourceItems = albums;
             });
+    }
+
+    function loadDefaultFeed(){
+        var slideshowSource = dataImages.flickr;
+        slideshowSource.images.load().then(loadImages);
     }
 
     function advanceImage(direction) {
